643642cf1f76ba16e47d9fbd86e1e65592c90fab
LaukikOnChromebook <22937426+LRagji@users.noreply.github.com>
2021-08-08 17:45:37 +0530

    Fixed Issues in lua script and added features to perf script

--- lua-scripts/enqueue-purge.lua
+++ lua-scripts/enqueue-purge.lua
@@ -1,23 +1,29 @@
 local recentActivityKey = KEYS[1]
 local purgeStreamKey = KEYS[2]
-local purgeThreshold = tonumber(ARGV[1])
+local purgeThreshold = math.floor(tonumber(ARGV[1])/1000)
 local epoch = tonumber(ARGV[2])
-local scanEntries = tonumber(ARGV[3]) *-1
+local scanEntries = tonumber(ARGV[3])-1
 local spaceKey = ARGV[4]
-local tempTime = redis.call("TIME")
-local currentTimestamp = tonumber(math.floor(tempTime[2])+(tempTime[1])*1000)
 local enquedIds = {}
 
-local lastElementWithScore = redis.call('ZRANGE',recentActivityKey,scanEntries,-1,'WITHSCORES')
-for i = 1, #lastElementWithScore,2 do
-    lastElementWithScore[i+1] = currentTimestamp - (lastElementWithScore[i+1] + epoch)
+local tempTime = redis.call("TIME")
+local currentTimestamp = tonumber(tempTime[1])
 
-    if(lastElementWithScore[i+1] > purgeThreshold) then 
+local lastElementWithScore = redis.call('ZRANGE',recentActivityKey,0,scanEntries,'WITHSCORES')
+for i = 1, #lastElementWithScore,2 do
+    local relativeActivityTime = currentTimestamp - math.floor((tonumber(lastElementWithScore[i+1]) + epoch)/1000)
+
+    if(relativeActivityTime > purgeThreshold) then 
         local dataToBePurged = redis.call('ZRANGE',(spaceKey ..lastElementWithScore[i]),0,-1,'WITHSCORES')
         local id = redis.call('XADD',purgeStreamKey,'*',lastElementWithScore[i],cjson.encode(dataToBePurged))
         redis.call('ZREM' ,recentActivityKey,lastElementWithScore[i])
         table.insert(enquedIds,id)
     end
+    -- table.insert(enquedIds,relativeActivityTime)
+    -- table.insert(enquedIds,lastElementWithScore[i+1])
+    -- table.insert(enquedIds,epoch)
+    -- table.insert(enquedIds,math.floor((tonumber(lastElementWithScore[i+1]) + epoch)/1000))
+    -- table.insert(enquedIds,currentTimestamp)
 end
 return enquedIds
 -- docker run with -v ${PWD}:\"/var/lib/mysql\"
