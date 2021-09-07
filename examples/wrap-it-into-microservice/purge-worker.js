ba38e21b5551a817ef1163a6f99f905a680fe528
LaukikOnChromebook <22937426+LRagji@users.noreply.github.com>
2021-09-08 01:08:27 +0530

    Updated Service

--- examples/wrap-it-into-microservice/purge-worker.js
+++ examples/wrap-it-into-microservice/purge-worker.js
@@ -2,24 +2,46 @@ const { parentPort, workerData } = require('worker_threads');
 const redisType = require("ioredis");
 const fs = require('fs').promises;
 const path = require('path');
-const timeseriesType = require("../../index").Timeseries;
+const timeseriesType = require("../../timeseries");
+const scripto = require('redis-scripto2');
+const hash = (tagName) => Array.from(tagName).reduce((acc, c) => acc + c.charCodeAt(0), 0);
+const tagToPartitionMapping = (tagName) => hash(tagName) % 10;
+//const partitionToRedisMapping = (partitionName) => redisClient;
+const tagNameToTagId = hash;
+const settings = {
+    "ActivityKey": "Activity",
+    "SamplesPerPartitionKey": "Stats",
+    "Seperator": "=",
+    "MaximumTagsInOneWrite": 2000,
+    "MaximumTagsInOneRead": 100,
+    "MaximumTagNameLength": 200,
+    "MaximumPartitionsScansInOneRead": 100,
+    "PartitionTimeWidth": 5000,
+    "PurgeMarker": "P"
+}
 
 async function mainSyncLoop(config) {
     const HotHoldTime = config.HotHoldTime;
     const holdTimeInSeconds = HotHoldTime / 1000;
     const coolDownTime = config.coolDownTime;
-    const processInOneLoop=config.processInOneLoop;
+    const processInOneLoop = config.processInOneLoop;
     const localRedisConnectionString = config.redisConnectionString;
     const redisClient = new redisType(localRedisConnectionString);
-    const store = new timeseriesType(redisClient);
-    await store.initialize();
+    const store = new timeseriesType(tagToPartitionMapping, (partitionName) => redisClient, tagNameToTagId, settings);
+    const scriptManager = new scripto(redisClient);
+    //await store.initialize();
 
     let shutdown = false;
     while (shutdown === false) {
         const startTime = Date.now();
         try {
             let summedAverage = 0.0;
-            const acquiredPartitions = await store.purgeAcquire((holdTimeInSeconds + 3), processInOneLoop, holdTimeInSeconds * 3);
+            const acquiredPartitions = await store.purgeAcquire(scriptManager, -1, 10000, (holdTimeInSeconds * 3), processInOneLoop);
+            acquiredPartitions.forEach((timeMap, tagId) => {
+
+            });
+
+
             for (let index = 0; index < acquiredPartitions.length; index++) {
                 const partitionInfo = acquiredPartitions[index];
                 let fileData = "";
--- examples/wrap-it-into-microservice/service.js
+++ examples/wrap-it-into-microservice/service.js
@@ -4,10 +4,25 @@ const compression = require('compression')
 const path = require('path');
 const { Worker } = require('worker_threads');
 const redisType = require("ioredis");
-const timeseriesType = require("../../index").Timeseries;
+const timeseriesType = require("../../timeseries");
 const localRedisConnectionString = "redis://127.0.0.1:6379/";
 const redisClient = new redisType(localRedisConnectionString);
-const store = new timeseriesType(redisClient);
+const hash = (tagName) => Array.from(tagName).reduce((acc, c) => acc + c.charCodeAt(0), 0);
+const tagToPartitionMapping = (tagName) => hash(tagName) % 10;
+const partitionToRedisMapping = (partitionName) => redisClient;
+const tagNameToTagId = hash;
+const settings = {
+    "ActivityKey": "Activity",
+    "SamplesPerPartitionKey": "Stats",
+    "Seperator": "=",
+    "MaximumTagsInOneWrite": 2000,
+    "MaximumTagsInOneRead": 100,
+    "MaximumTagNameLength": 200,
+    "MaximumPartitionsScansInOneRead": 100,
+    "PartitionTimeWidth": 5000,
+    "PurgeMarker": "P"
+}
+const store = new timeseriesType(tagToPartitionMapping, partitionToRedisMapping, tagNameToTagId, settings);
 const app = express()
 const port = 3000
 const HotHoldTime = 120 * 1000;
@@ -27,11 +42,13 @@ app.post('/set', async (req, res) => {
             });
             data.set(tagName, orderedData);
         });
-        const bytes = await store.write(data);
-        // if (bytes > BigInt(purgeLimit)) {
-        //     await store.purgeScan(5000, 1000);
-        // }
-        res.status(200).json(bytes.toString());
+        const response = await store.write(data);
+        if (response === true) {
+            res.status(204).end();
+        }
+        else {
+            res.status(400).json({ "error": "Failed to save data." });
+        }
     }
     catch (err) {
         res.status(500).json(err.stack);
@@ -46,70 +63,70 @@ app.post('/get', (req, res) => {
         range.end = BigInt(req.body[tagName].end);
         ranges.set(tagName, range);
     });
-    readData(ranges)
+    store.read(ranges)
         .then(result => res.json(result))
         .catch(error => res.status(500).json(JSON.stringify(error)));
 });
 
-async function readData(ranges) {
-    //READ Indexes
-    const pages = await store.readIndex(ranges);
-    const tagNames = Array.from(ranges.keys());
+// async function readData(ranges) {
+//     //READ Indexes
+//     const pages = await store.readIndex(ranges);
+//     const tagNames = Array.from(ranges.keys());
 
-    //READ Pages
-    let asyncCommands = [];
-    pages.forEach((pages, partitionName) => {
-        pages.forEach((page) => {
-            asyncCommands.push((async () => {
-                const sortedMap = await store.readPage(page.page, (sortKey, tagName) => tagNames.indexOf(tagName) > -1 && page.start <= sortKey && sortKey <= page.end);
-                return new Map([[partitionName, sortedMap]]);
-            })());
-        });
-    });
-    let queryResults = await Promise.allSettled(asyncCommands);
-    const result = new Map();
-    queryResults.reverse().forEach((e) => {
-        if (e.status !== "fulfilled") {
-            throw new Error(e.reason);
-        }
-        e = e.value;
-        const partitionName = Array.from(e.keys())[0];
-        const existingData = result.get(partitionName) || new Map();
-        e.get(partitionName).forEach((v, k) => existingData.set(k, v));
-        result.set(partitionName, existingData);
-    });
-    let returnObject = {};
-    result.forEach((v, k) => {
-        returnObject[k] = Object.fromEntries(v);
-    });
-    return returnObject;
-}
+//     //READ Pages
+//     let asyncCommands = [];
+//     pages.forEach((pages, partitionName) => {
+//         pages.forEach((page) => {
+//             asyncCommands.push((async () => {
+//                 const sortedMap = await store.readPage(page.page, (sortKey, tagName) => tagNames.indexOf(tagName) > -1 && page.start <= sortKey && sortKey <= page.end);
+//                 return new Map([[partitionName, sortedMap]]);
+//             })());
+//         });
+//     });
+//     let queryResults = await Promise.allSettled(asyncCommands);
+//     const result = new Map();
+//     queryResults.reverse().forEach((e) => {
+//         if (e.status !== "fulfilled") {
+//             throw new Error(e.reason);
+//         }
+//         e = e.value;
+//         const partitionName = Array.from(e.keys())[0];
+//         const existingData = result.get(partitionName) || new Map();
+//         e.get(partitionName).forEach((v, k) => existingData.set(k, v));
+//         result.set(partitionName, existingData);
+//     });
+//     let returnObject = {};
+//     result.forEach((v, k) => {
+//         returnObject[k] = Object.fromEntries(v);
+//     });
+//     return returnObject;
+// }
 
 //Startup
 (async () => {
-    await store.initialize();
+    //await store.initialize();
 
     const consumerName = `C-${store.instanceName}`;
     app.listen(port, () => {
         console.log(`${consumerName} listening at http://localhost:${port}`);
     });
 
-    await new Promise((resolve, reject) => {
-        const worker = new Worker(path.join(__dirname, "purge-worker.js"), {
-            workerData: {
-                "HotHoldTime": HotHoldTime,
-                "coolDownTime": 5000,
-                "processInOneLoop": 200,
-                "redisConnectionString": localRedisConnectionString
-            }
-        });
-        worker.on('message', resolve);
-        worker.on('error', reject);
-        worker.on('exit', (code) => {
-            if (code !== 0)
-                reject(new Error(`Worker stopped with exit code ${code}`));
-        });
-    });
+    // await new Promise((resolve, reject) => {
+    //     const worker = new Worker(path.join(__dirname, "purge-worker.js"), {
+    //         workerData: {
+    //             "HotHoldTime": HotHoldTime,
+    //             "coolDownTime": 5000,
+    //             "processInOneLoop": 200,
+    //             "redisConnectionString": localRedisConnectionString
+    //         }
+    //     });
+    //     worker.on('message', resolve);
+    //     worker.on('error', reject);
+    //     worker.on('exit', (code) => {
+    //         if (code !== 0)
+    //             reject(new Error(`Worker stopped with exit code ${code}`));
+    //     });
+    // });
 
 })()
     .then(consumerName => {
